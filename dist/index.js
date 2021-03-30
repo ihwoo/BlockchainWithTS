6cdefe7380a9978d299c9a19c2d04d0a15cf6d05
ihwoo <inaj012@gmail.com>
2021-03-30 15:41:22 +0900

    Sample project with TypeScript

---
+++ dist/index.js
@@ -0,0 +1,64 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+const genesisBlock = new Block(0, "123412331", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const nextHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, nextHash, previousBlock.hash, data, newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        console.log('Structure err');
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        console.log('idx err');
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        console.log('prev hash err');
+        return false;
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        console.log('hash err');
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
+createNewBlock("Second block");
+createNewBlock("Third block");
+createNewBlock("Forth block");
+console.log(blockchain);
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var sayHi = function (name, age, gender) {
+    return 'Hello ' + name + ', you are ' + age + gender;
+};
+console.log(sayHi("ihwoo", 36, "male"));
